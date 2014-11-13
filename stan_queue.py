1b2a45646edb4c84b9f9dba01b72b579b33302ca
Stanimir Angelov <C12733849@mydit.ie>
2014-11-13 18:17:36 +0200

    Adding new files to repo

--- stan_queue.py
+++ stan_queue.py
@@ -1,6 +1,5 @@
-# This script created a queue
+#This script created a queue
 #
-# Author - Paul Doyle Aug 2013
 #
 #
 import boto.sqs
@@ -11,6 +10,8 @@ from boto.exception import SQSError
 
 conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='AKIAIR7EH3TNSTDUCWKA', aws_secret_access_key='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')
 
-rs = conn.get_all_queues()
-for q in rs:
-  print q.id
+q = conn.create_queue('Stanimir_Queue')
+
+m = Message()
+m.set_body('My message is that I finally catch up')
+q.write(m)
